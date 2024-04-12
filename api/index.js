5dc091fcf6a9840afc928c8512f65c1d1d397788
HussainGawhari <hussain.gawhari2018@gmail.com>
2024-04-12 13:44:43 +0530

    Database connection connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose
+    .connect(process.env.MONGO)
+    .then(
+        console.log(" Mongoose Connect successfully")
+    ).catch((err) => {
+        console.log(err);
+    });
+
 const app = express();
 app.listen(3000, () => {
     console.log('listening on post 3000!!');
