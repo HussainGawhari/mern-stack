9b9e0f8bd6cc9f920540c08c1f97ad0e969d3c66
HussainGawhari <hussain.gawhari2018@gmail.com>
2024-05-01 17:14:28 +0530

    sign in functionality completed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const signUp = async(req, res, next) => {
     const { username, email, password } = req.body;
@@ -27,5 +28,34 @@ export const signUp = async(req, res, next) => {
         res.json("user successfully added")
     } catch (err) {
         next(err);
+    }
+}
+
+export const signIn = async(req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password || email === '' || password === '') {
+        return next(errorHandler(400, 'Invalid email or password'));
+
+    }
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found'));
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(400, 'Password incorrect'));
+        }
+        const token = jwt.sign({ id: validUser._id },
+            process.env.JWT_SECRET)
+
+        const { password: pas, ...rest } = validUser._doc;
+        res.status(200).cookie('access_token', token, {
+                hhtponly: true,
+            })
+            .json(rest);
+    } catch (err) {
+        return next(err);
     }
 }
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+
 import Dashboard from './pages/Dashboard';
 import About from './pages/About';
 import Project from './pages/Project';
 import Header from './components/Header';
 import FooterComponent from './components/Footer';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+
 
 
 export default function App() {
